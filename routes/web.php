c26f59049a772033b249e801a68f92ea6713c4f0
shiho <gee.kwus@gmail.com>
2019-09-11 16:55:46 +0900

    課題１３

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,23 @@
 Route::get('/',function(){
   return view('welcome');
   });
-   
+
   //10.3課題
   Route::get('XXX','AAAController@bbb');
   //10.4課題
   Route::group(['prefix' => 'admin'],function(){
-   Route::get('profile/create','Admin\ProfileController@add');
-   Route::get('profile/edit','Admin\ProfileController@edit');
+   Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+   Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
    Route::get('news/create','Admin\NewsController@add');
   });
-           
 
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//ログインしていない時のリダイレクトの処理
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+});
